af8298751132190b387aab6901ed0246152b32a7
Steve Dondley <s@dondley.com>
2020-07-05 11:27:30 -0400

    ignore shell.sh

--- test/shell.sh
+++ test/shell.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+GITHUB_ACTOR=sdondley
+GITHUB_REPOSITORY=sdondley/sdondley.github.io
+BRANCH=gh-pages
+GITHUB_TOKEN=0d407fc52bdb68ec9ca1334b9a02f3aedac1782f
 
 DEST="${JEKYLL_DESTINATION:-_site}"
 REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
@@ -25,4 +29,3 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "published by GitHub Actions"
 git push --force ${REPO} master:${BRANCH}
-
